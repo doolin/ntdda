32667334b2ae2f2c125e184f5b648ccb66e79559
doolin <doolin>
2002-05-28 01:17:35 +0000

    forgot to add a headef file

---
+++ include/inpoly.h
@@ -0,0 +1,32 @@
+
+
+#ifndef __INPOLY_H__
+#define __INPOLY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+#if 0
+}
+#endif
+
+
+int pointinpoly  (int blocknumber, 
+                  double x11, 
+                  double y11,
+                  int ** vindex, 
+                  double ** vertices);
+
+
+
+int inpoly        (double ** poly,
+                   int npoints,
+                   double xt,
+                   double yt);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __INPOLY_H__ */
