237779aad240de1887aee6329c561c23d8677c28
doolin <doolin>
2002-10-27 12:45:26 +0000

    Adding units test for point in polygon function.

---
+++ src/unittests/inpoly_test.c
@@ -0,0 +1,34 @@
+
+/**
+ * Unit test code for point-in-polygon functions.
+ */
+
+#include <stdio.h>
+
+#include "inpoly.h"
+
+
+int
+test_inpoly(void) {
+
+   return 0;
+}
+
+
+int 
+inpoly_test(void) {
+
+  test_inpoly();
+
+  return 0;
+
+}
+
+
+int 
+main() {
+
+  inpoly_test();
+
+   return 0;
+}
