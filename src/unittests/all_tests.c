c58fa8715037ffb30d2340a5744a9235d62c6782
doolin <doolin>
2002-10-31 15:36:28 +0000

    Forgot to add a couple of test files.

---
+++ src/unittests/all_tests.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+
+int 
+main() {
+
+   constants_test();
+   bolt_test();
+
+   return 0;
+
+}
---
+++ src/unittests/functions_test.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+
+
+#ifdef STANDALONE
+int
+main() {
+
+   return 0;
+}
+#endif
