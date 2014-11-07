7f56860fedec3105f59de7efa0e8151504b083cc
Dave Doolin <david.doolin@gmail.com>
2014-11-07 05:58:49 -0800

    cleanup

--- src/unittests/analysisdata_test.c
+++ src/unittests/analysisdata_test.c
@@ -7,63 +7,44 @@
 FILEPOINTERS fp;
 Datalog * DLog;
 
-
 void
 dda_display_error(const char * error) {
-
 }
 
-void 
+void
 ddaml_read_analysis_file(void * userdata, char * filename) {
-
-
 }
-
-
 
 /** See if we are releasing all the memory. */
-int 
+int
 test_adata_new(void) {
 
   Analysisdata * ad = adata_new();
-
   adata_delete(ad);
-
   return 0;
 }
-
 
 int
 test_adata_clone(void) {
-
   Analysisdata * ad1, * ad2;
-  
   ad1 = adata_new();
-
   ad2 = adata_clone(ad1);
-
   adata_delete(ad1);
   adata_delete(ad2);
-
   return 0;
 }
-
-
 
 int
 analysisdata_test(void) {
 
   test_adata_new();
   test_adata_clone();
-
   return 0;
 }
 
 
-int 
+int
 main() {
-
   analysisdata_test();
-
   return 0;
 }
